ad017a71b63dae953df497867d7578803f65fc9b
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-11-29 21:07:44 +0100

    enable ethereum

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with metamask we have an access to window.ethereum & to window.web3
+      // metamask injexts a global API into website
+      // this API allows websites to request users, accounts, read data to blockchain,
+      // sign messages and transactions
+
+      console.log(window.web3);
+      console.log(window.ethereum);
+    };
+
+    loadProvider();
+  }, []);
+
   return (
     <>
       <div className="faucet-wrapper">
@@ -9,7 +24,17 @@ function App() {
             Current balance: <strong>10</strong> ETH
           </div>
           <div className="faucet-buttons">
-            <button className="button mr-2">Donate</button>
+            <button
+              className="button mr-2"
+              onClick={async () => {
+                const accounts = await window.ethereum.request({
+                  method: "eth_requestAccounts",
+                });
+                console.log(accounts);
+              }}
+            >
+              Donate
+            </button>
             <button className="button">Withdraw</button>
           </div>
         </div>
