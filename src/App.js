19d461234b77eb8b69dcd0572bfc0797bd6fab46
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-11-29 20:59:04 +0100

    added some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current balance: <strong>10</strong> ETH
+          </div>
+          <div className="faucet-buttons">
+            <button className="button mr-2">Donate</button>
+            <button className="button">Withdraw</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
