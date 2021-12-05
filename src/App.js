8cd8f46a86fd6bc271a88d4adf8e02c6ac17d4df
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-05 16:30:48 +0100

    get account

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Web3 from "web3";
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    wen3: null,
+    web3: null,
   });
+  const [account, setAccount] = useState(null);
 
   useEffect(() => {
     const loadProvider = async () => {
@@ -31,10 +32,22 @@ function App() {
     loadProvider();
   }, []);
 
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3Api.web3 && getAccounts();
+  }, [web3Api.web3]);
+
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          <h1>{account ? account : "Not connected"}</h1>
           <div className="balance-view is-size-2">
             Current balance: <strong>10</strong> ETH
           </div>
